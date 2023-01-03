5a55d42cec6cace61965622edd5d91b821981b60
Aron Jon Prenovost <Aroniascoder@gmail.com>
2023-01-03 06:30:48 -0800

    updates

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const msg = {
   to: 'aronprenovostmktg@gmail.com', // Change to your recipient
-  from: 'test@example.com', // Change to your verified sender
+  from: 'aronprenovostmktg@gmail.com', // Change to your verified sender
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
